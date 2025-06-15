8d2c2f48250e0cc4043cf8b38658ce8119254638
scherepi <48649270+scherepi@users.noreply.github.com>
2025-06-15 06:06:42 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { render } from "svelte/server";
+    let { children } = $props();
+    import "../app.css";
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    console.log("Troubadour v1.0.0, started up");
+</script>
+<style lang="postcss">
+    @reference "tailwindcss";
+
+    :global(html) {
+        background-color: #cfcece;
+    }
+
+    h1 {
+        color: slategray;
+        font-family: 'Times New Roman', 'serif';
+        justify-content: center;
+        size: 90px;
+    }
+</style>
+<h1 class="text-3xl font-bold underline">Troubadour</h1>
+<h4 id="subtitle">Your music, annotated your way.</h4>
