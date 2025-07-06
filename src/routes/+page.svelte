7de6216140f9e99099861f15d9272ab7c69c190b
scherepi <48649270+scherepi@users.noreply.github.com>
2025-07-06 15:24:19 -0500

    Put together login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,26 @@
     console.log("Troubadour v1.0.0, started up");
 </script>
 <style lang="postcss">
+    @import url('https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200..900;1,200..900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Schibsted+Grotesk:ital,wght@0,400..900;1,400..900&display=swap');
     @reference "tailwindcss";
+
+    @theme {
+        --font-script: 'Schibsted Grotesk', serif;
+        --font-serif: 'Crimson Pro', serif;
+        --text-shadow-cyan-900: 0  35px 10px rgba(0, 255, 255, 0.5);
+    }
 
     :global(html) {
         background-color: #cfcece;
+        font-family: 'Crimson Pro', serif;
     }
-
-    h1 {
-        color: slategray;
-        font-family: 'Times New Roman', 'serif';
-        justify-content: center;
-        size: 90px;
+    a {
+        font-family: 'Times New Roman', serif;
     }
 </style>
-<h1 class="text-3xl font-bold underline">Troubadour</h1>
-<h4 id="subtitle">Your music, annotated your way.</h4>
+<h1 class="text-8xl font-bold text-gray-600 text-center -inset-y-3 mt-50 text-shadow-cyan-900/50 ">Troubadour</h1>
+<h4 class="text-2xl font-medium text-gray-500 text-center mt-2">Your music, annotated your way.</h4>
+<div class="text-center bg-gray-800 p-4 shadow-lg rounded-2xl w-35 mt-10 ml-146 hover:bg-gray-700">
+    <a href="/login" class="text-center text-gray-200 font-script p-4 m-4">Enter</a>
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
+
+    let username: string = $state("");
+    let password: string = $state("");
+    let fadeController: boolean = $state(false)
+    $inspect(username).with(console.trace);
+    $inspect(password).with(console.trace)
+    onMount(() => {
+        fadeController = true;
+    })
+
+    function onclick() {
+        // Submit query to database using username and password here!
+    }
+</script>
+<style lang="postcss">
+    @import url('https://fonts.googleapis.com/css2?family=Schibsted+Grotesk:ital,wght@0,400..900;1,400..900&display=swap');
+    @reference "tailwindcss";
+    @theme {
+        --font-script: 'Schibsted Grotesk', serif;
+    }
+
+    :global(html) {
+        background-color: #cfcece;
+    }
+</style>
+
+<div class="rounded-2xl mt-50 p-0 flex flex-col items-center text-center justify-center mx-auto w-md max-w-md">
+    {#if fadeController}<h1 class="text-7xl text-center m-0 p-0 font-bold italic font-[Schibsted_Grotesk]" transition:fade>Login</h1> {/if}
+    <input type="text" class="bg-gray-300 border-2 inline-block rounded-md w-60 mt-10" id="user" bind:value={username} placeholder="username">
+    <input type="password" class="bg-gray-300 border-2 inline-block rounded-md w-60 mt-2" id="password" bind:value={password} placeholder="password">
+    <input type="button" class="bg-blue-500 border-2 inline-block rounded-full w-40 mt-5 font-[Schibsted_Grotesk]" value="Enter" {onclick}>
+</div>
+
+<footer></footer>
