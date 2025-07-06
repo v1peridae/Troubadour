7b4e085dab973619667c3f944f649106a917d317
scherepi <48649270+scherepi@users.noreply.github.com>
2025-07-06 15:45:52 -0500

    Refined transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
+
     console.log("Troubadour v1.0.0, started up");
+    let fadeController: boolean = $state(false);
+    onMount(() => {
+        fadeController = true;
+    })
 </script>
 <style lang="postcss">
     @import url('https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200..900;1,200..900&display=swap');
@@ -21,7 +28,7 @@
     }
 </style>
 <h1 class="text-8xl font-bold text-gray-600 text-center -inset-y-3 mt-50 text-shadow-cyan-900/50 ">Troubadour</h1>
-<h4 class="text-2xl font-medium text-gray-500 text-center mt-2">Your music, annotated your way.</h4>
-<div class="text-center bg-gray-800 p-4 shadow-lg rounded-2xl w-35 mt-10 ml-146 hover:bg-gray-700">
-    <a href="/login" class="text-center text-gray-200 font-script p-4 m-4">Enter</a>
+{#if fadeController}<h4 id="subtitle" class="text-2xl font-medium text-gray-500 text-center mt-2" transition:fade={{duration: 3000}}>Your music, annotated your way.</h4> {/if}
+<div class="text-center bg-gray-800 p-4 shadow-lg rounded-2xl w-35 mt-6 ml-146 hover:bg-gray-700">
+    <a href="/login" class="text-center text-gray-200 font-[Schibsted Grotesk] italic font-light p-4 m-4">Enter</a>
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -28,9 +28,9 @@
 </style>
 
 <div class="rounded-2xl mt-50 p-0 flex flex-col items-center text-center justify-center mx-auto w-md max-w-md">
-    {#if fadeController}<h1 class="text-7xl text-center m-0 p-0 font-bold italic font-[Schibsted_Grotesk]" transition:fade>Login</h1> {/if}
-    <input type="text" class="bg-gray-300 border-2 inline-block rounded-md w-60 mt-10" id="user" bind:value={username} placeholder="username">
-    <input type="password" class="bg-gray-300 border-2 inline-block rounded-md w-60 mt-2" id="password" bind:value={password} placeholder="password">
+    {#if fadeController}<h1 class="text-7xl text-center m-0 p-0 font-bold italic font-[Schibsted_Grotesk] opacity-85" transition:fade>Login</h1> {/if}
+    <input type="text" class="bg-gray-300 border-2 inline-block rounded-md w-60 mt-6 items-center text-center justify-center" id="user" bind:value={username} placeholder="username">
+    <input type="password" class="bg-gray-300 border-2 inline-block rounded-md w-60 mt-2 items-center text-center justify-center" id="password" bind:value={password} placeholder="password">
     <input type="button" class="bg-blue-500 border-2 inline-block rounded-full w-40 mt-5 font-[Schibsted_Grotesk]" value="Enter" {onclick}>
 </div>
 
